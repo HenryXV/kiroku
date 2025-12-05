4a935b1736e3202b8c821fbc09f6463313200816
henry <henri.r.s@hotmail.com>
2025-12-05 17:36:50 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { errorHandler } from "./api/middlewares/errorHandler.js";
 import routes from "./api/routes/index.js";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
